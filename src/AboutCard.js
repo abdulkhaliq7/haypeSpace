ae9bcdbe0a512fa6303a85d5446d95a114e8167f
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2020-11-21 13:46:54 +0800

    aboutCard

---
+++ src/AboutCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './AboutCard.css'
+
+function AboutCard() {
+    return (
+        <div className='about__card'>
+            <img className='about__image' src='https://picsum.photos/seed/picsum/200/300' />
+            <div className="about__discription">
+                <p className="about__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Rem numqulabore, id iusto, nam tempora a ducimus blanditiis dolorem officia.
+                </p>
+                <p className="about__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Nesciunt officia cum distinctio hic velit minus n
+                </p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default AboutCard
+
+
+
+
+
+
+
+
+
---
+++ src/AboutUs.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import AboutCard from './AboutCard'
+import './AboutUs.css'
+
+function AboutUs() {
+    return (
+        <div className='aboutUs'>
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+        </div>
+    )
+}
+
+export default AboutUs
